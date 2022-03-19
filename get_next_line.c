972ade75cfd8cf370b2748d50647412fff86bf49
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-19 21:35:11 +0100

    start of the function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 22:12:47 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/18 22:17:03 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/19 21:32:24 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 char	*get_next_line(int fd)
 {
+	int		n;
+	char	*buf;
 
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	n = read(fd, buf, BUFFER_SIZE);
+	if (n < 0)
+		return (NULL);
+	
 }
+
