8f73e43403f74d6cd84501b4f3adf4a34120515e
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-19 21:34:34 +0100

    Add libraries + a constant

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 22:13:40 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/18 22:15:19 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/19 16:31:55 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,19 @@
 
 # define GET_NEXT_LINE_H
 
-// Libraries
+/*
+**	Libraries
+*/
+
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+/*
+**	Define constants
+*/
+
+# define BUFFER_SIZE 42
 
 #endif
