3dacebba14e69814e8a0407c57c0d501368fc5f7
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-21 20:00:34 +0100

    get_next_line progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,50 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 22:12:47 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/19 21:32:24 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/21 19:59:35 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*ft_strjoin_gnl(char *s1, char *s2)
+{
+	char	*dst;
+	
+	if (!s1)
+	{
+		dst = (char *)malloc(sizeof(char) * (ft_strlen(s2) + 1));
+		if (!dst)
+			return (NULL);
+		ft_strcpy(dst, s2);
+		return (dst);
+	}
+	dst = ft_strcat(s1, s2);
+	return (dst);
+}
+
 char	*get_next_line(int fd)
 {
-	int		n;
-	char	*buf;
+	static char	*str;
+	int			n;
+	char		*buf;
+	int			i;
 
+	i = 0;
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
 	n = read(fd, buf, BUFFER_SIZE);
 	if (n < 0)
 		return (NULL);
-	
+	str = ft_strjoin_gnl(str, buf);
+	while (!is_return(buf))
+	{
+		n = read(fd, buf, BUFFER_SIZE);
+		if (n < 0)
+			return (NULL);
+		str = ft_strjoin_gnl(str, buf);
+		i++;
+	}
+	return (str);
 }
-
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 22:13:40 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/19 16:31:55 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/21 19:58:55 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,15 @@
 **	Define constants
 */
 
-# define BUFFER_SIZE 42
+# define BUFFER_SIZE 10
+
+/*
+**	Utils functions
+*/
+
+int		ft_strlen(char *str);
+int		is_return(char *str);
+char	*ft_strcat(char *dest, char *src);
+char	*ft_strcpy(char *dest, char *src);
 
 #endif
