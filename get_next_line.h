5804e33b8f6779e0251b730ad90aa22d6ab63cd3
lirockk <arthurdemurger26@gmail.com>
2022-04-19 13:51:34 +0200

    Putting the prototypes in order

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 22:13:40 by ademurge          #+#    #+#             */
-/*   Updated: 2022/04/19 00:29:14 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/04/19 13:48:01 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,20 @@
 # include <unistd.h>
 # include <fcntl.h>
 
-char	*get_next_line(int fd);
-
-/*
-**	Define constants
-*/
-
-# define BUFFER_SIZE 42
-
 /*
 **	Utils functions
 */
 
+int		is_line_break(char *str);
 int		ft_strlen(char *s);
 char	*ft_strdup(char *src);
 char	*gnl_strjoin(char *s1, char *s2);
-int		is_line_break(char *str);
 char	*gnl_substr(char const *s, int start, int len);
+
+/*
+** get_next_line function
+*/
+
+char	*get_next_line(int fd);
 
 #endif
